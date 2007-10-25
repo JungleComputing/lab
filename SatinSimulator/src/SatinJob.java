ae35075a7af7956b443781285eb23ee1cef6c7be
Kees van Reeuwijk <keesvr@xs4all.nl>
2007-10-25 08:54:48 +0000

    + Work stealing works again.

--- SatinSimulator/src/SatinJob.java
+++ SatinSimulator/src/SatinJob.java
@@ -4,91 +4,91 @@ import desmoj.core.simulator.SimTime;
 /** A job. */
 public class SatinJob extends SimProcess
 {
-    private SatinProcessor processor;
-    private final SatinSimulator model;
-    private final int depth;
-    private static final boolean TRACE_JOBS = true;
-    private final SatinJob parent;
-    private final int childNo;
-    private boolean[] childrenReady;
+	private SatinProcessor processor;
+	private final SatinSimulator model;
+	private final int depth;
+	private static final boolean TRACE_JOBS = true;
+	private final SatinJob parent;
+	private final int childNo;
+	private boolean[] childrenReady;
 
-    /**
-     * Constructs a new SatinJob.
-     * @param model The model this job belongs to.
-     * @param name The name of the job
-     * @param trace Trace execution of this job?
-     * @param processor The processor the job belongs to.
-     * @param depth The recursion depth of the satin jobs.
-     * @param parent Who is the parent of this job?
-     * @param childNo What is the child number of this job?
-     */
-    public SatinJob( SatinSimulator model, String name, boolean trace, SatinProcessor processor, int depth, SatinJob parent, int childNo )
-    {
-	super( model, name, trace );
-	this.processor = processor;
-	this.model = model;
-	this.depth = depth;
-	this.parent = parent;
-	this.childNo = childNo;
-    }
+	/**
+	 * Constructs a new SatinJob.
+	 * @param model The model this job belongs to.
+	 * @param name The name of the job
+	 * @param trace Trace execution of this job?
+	 * @param processor The processor the job belongs to.
+	 * @param depth The recursion depth of the satin jobs.
+	 * @param parent Who is the parent of this job?
+	 * @param childNo What is the child number of this job?
+	 */
+	public SatinJob( SatinSimulator model, String name, boolean trace, SatinProcessor processor, int depth, SatinJob parent, int childNo )
+	{
+		super( model, name, trace );
+		this.processor = processor;
+		this.model = model;
+		this.depth = depth;
+		this.parent = parent;
+		this.childNo = childNo;
+	}
 
-    private boolean childrenAreReady()
-    {
-	if( childrenReady == null ) {
-	    return true;
+	private boolean childrenAreReady()
+	{
+		if( childrenReady == null ) {
+			return true;
+		}
+		for( boolean ready: childrenReady ) {
+			if( !ready ) {
+				return false;
+			}
+		}
+		return true;
 	}
-	for( boolean ready: childrenReady ) {
-	    if( !ready ) {
-		return false;
-	    }
+	/**
+	 * The execution cycle of a Satin job. (Overrides method in superclass.)
+	 */
+	@Override
+	public void lifeCycle()
+	{
+		hold( new SimTime( model.getPreSpawnExecutionTime( processor.getSlowdown() ) ) );
+		if( depth>0 ){
+			SatinJob joba = new SatinJob( model, "job" + depth + "a", TRACE_JOBS, processor, depth-1, this, 0 );
+			processor.queueJob( joba );
+			SatinJob jobb = new SatinJob( model, "job" + depth + "b", TRACE_JOBS, processor, depth-1, this, 1 );
+			processor.queueJob( jobb );
+			childrenReady = new boolean[2];
+		}
+		double postSpawnExecutionTime = model.getPostSpawnExecutionTime( processor.getSlowdown() );
+		hold( new SimTime( postSpawnExecutionTime ) );
+		processor.activateAfter( this );
+		while( !childrenAreReady() ) {
+			passivate();
+		}
+		if( parent == null ){
+			sendTraceNote( "Root job has finished" );
+			model.rootHasFinished();
+		}
+		else {
+			parent.registerChildCompleted( childNo );
+		}
 	}
-	return true;
-    }
-    /**
-     * The execution cycle of a Satin job. (Overrides method in superclass.)
-     */
-    @Override
-    public void lifeCycle()
-    {
-	hold( new SimTime( model.getPreSpawnExecutionTime( processor.getSlowdown() ) ) );
-	if( depth>0 ){
-	    SatinJob joba = new SatinJob( model, "job" + depth + "a", TRACE_JOBS, processor, depth-1, this, 0 );
-	    processor.queueJob( joba );
-	    SatinJob jobb = new SatinJob( model, "job" + depth + "b", TRACE_JOBS, processor, depth-1, this, 1 );
-	    processor.queueJob( jobb );
-	    childrenReady = new boolean[2];
-	}
-	double postSpawnExecutionTime = model.getPostSpawnExecutionTime( processor.getSlowdown() );
-	hold( new SimTime( postSpawnExecutionTime ) );
-	processor.activateAfter( this );
-	while( !childrenAreReady() ) {
-	    passivate();
-	}
-	if( parent == null ){
-	    sendTraceNote( "Root job has finished" );
-	    model.rootHasFinished();
-	}
-	else {
-	    parent.registerChildCompleted( childNo );
-	}
-    }
 
-    /** Assign this job to the given processor.
-     * @param p The new owner of this job.
-     */
-    public void setProcessor( SatinProcessor p )
-    {
-	processor = p;
-    }
+	/** Assign this job to the given processor.
+	 * @param p The new owner of this job.
+	 */
+	public void setProcessor( SatinProcessor p )
+	{
+		processor = p;
+	}
 
-    /** Tell the job that the child with the given number is ready.
-     * 
-     * @param no The child that is ready.
-     */
-    public void registerChildCompleted( int no )
-    {
-	childrenReady[no] = true;
-	activate( new SimTime( 0.0 ) );
-    }
+	/** Tell the job that the child with the given number is ready.
+	 * 
+	 * @param no The child that is ready.
+	 */
+	public void registerChildCompleted( int no )
+	{
+		childrenReady[no] = true;
+		activate( new SimTime( 0.0 ) );
+	}
 
 }
--- SatinSimulator/src/SatinProcessor.java
+++ SatinSimulator/src/SatinProcessor.java
@@ -5,100 +5,105 @@ import desmoj.core.simulator.SimTime;
 /**
  * @author Kees van Reeuwijk
  *
+ * A Satin processor.
  */
 public class SatinProcessor extends SimProcess
 {
-    private SatinSimulator model;
-    private SatinProcessor processors[];
-    private ProcessQueue workQueue;
-    private final double slowdown;
-    private double idleTime;
-    private final int procno;
+	private SatinSimulator model;
+	private SatinProcessor processors[];
+	private ProcessQueue workQueue;
+	private final double slowdown;
+	private double idleTime;
+	private final int procno;
 
-    /** constructs a process... 
-     * @param model The model the process belongs to.
-     * @param processors The processors that participate in the computation.
-     * @param procno The processor number.
-     * @param slowdown The computation time multiplier of this processor.
-     */
-    public SatinProcessor( SatinSimulator model, SatinProcessor processors[], int procno, double slowdown )
-    {
-        super( model, "P"+procno, true );
-        this.model = model;
-        this.processors = processors;
-        this.procno = procno;
-        this.slowdown = slowdown;
-        workQueue = new ProcessQueue( model, "job queue P" + procno, true, true );
-        idleTime = 0.0;
-    }
-
-    /** describes this process's life cycle */
-    @Override
-    public void lifeCycle()
-    {
-        while( true ){
-            SatinJob job = (SatinJob) workQueue.last();
-            if( job != null ){
-                workQueue.remove( job );
-                sendTraceNote( "Executing job " + job );
-                job.activateAfter( this );
-                passivate();
-            }
-            else {
-                int victim = model.getStealVictim( procno );
-                if( victim<0 ) {
-                    break;
-                }
-                stealWork( processors[victim] );
-                passivate();
-            }
-        }
-    }
-
-    private void stealWork( SatinProcessor victim )
-    {
-	SatinJob job = (SatinJob) victim.workQueue.first();
-	if( job == null ) {
-	    double sleepTime = 0.1;
-	    
-	    idleTime += sleepTime;
-	    activate( new SimTime( sleepTime ) );
+	/** constructs a process... 
+	 * @param model The model the process belongs to.
+	 * @param processors The processors that participate in the computation.
+	 * @param procno The processor number.
+	 * @param slowdown The computation time multiplier of this processor.
+	 */
+	public SatinProcessor( SatinSimulator model, SatinProcessor processors[], int procno, double slowdown )
+	{
+		super( model, "P"+procno, true );
+		this.model = model;
+		this.processors = processors;
+		this.procno = procno;
+		this.slowdown = slowdown;
+		workQueue = new ProcessQueue( model, "job queue P" + procno, true, true );
+		idleTime = 0.0;
 	}
-	else {
-	    job.setProcessor( this );
-	    queueJob( job );
-            sendTraceNote(  "steals a job " + job + " from P" + victim.procno );
-            double stealTime = model.getStealTime();
-            activate( new SimTime( 0.0 ) );
+
+	/** describes this process's life cycle */
+	@Override
+	public void lifeCycle()
+	{
+		while( true ){
+			SatinJob job = (SatinJob) workQueue.last();
+			if( job != null ){
+				workQueue.remove( job );
+				sendTraceNote( "Executing job " + job );
+				job.activateAfter( this );
+				passivate();
+			}
+			else {
+				int victim = model.getStealVictim( procno );
+				if( victim<0 ) {
+					break;
+				}
+				stealWork( processors[victim] );
+				passivate();
+			}
+		}
 	}
-    }
 
-    /** Put the given job on the work queue.
-     * @param job The job to queue.
-     */
-    public void queueJob( SatinJob job )
-    {
-        workQueue.insert( job );
-    }
+	private void stealWork( SatinProcessor victim )
+	{
+		System.out.println( "P" + procno + ": try to steal a job from P" + victim.procno );
+		SatinJob job = (SatinJob) victim.workQueue.first();
+		if( job == null ) {
+			double sleepTime = 0.1;
 
-    /**
-     * Returns the idle time of this processor.
-     * @return The idle time.
-     */
-    public double getIdleTime()
-    {
-	return idleTime;
-    }
+			idleTime += sleepTime;
+			activate( new SimTime( sleepTime ) );
+		}
+		else {
+			victim.workQueue.remove( job );
+			job.setProcessor( this );
+			queueJob( job );
+			System.out.println(  "P" + procno + ": steals a job " + job + " from P" + victim.procno );
+			sendTraceNote(  "steals a job " + job + " from P" + victim.procno );
+			double stealTime = model.getStealTime();
+			activate( new SimTime( 0.0 ) );
+			//activate( new SimTime( stealTime ) );
+		}
+	}
 
-    /** Returns the slowdown of this processor. The slowdown is
-     * the multiplication factor for execution times on this processor.
-     * Slow processors, compared to the reference, have a factor
-     * above 1, fast processors have a slowdown below 1.
-     * 
-     * @return The slowdown of this processor.
-     */
-    public double getSlowdown()
-    {
-	return slowdown;
-    }
+	/** Put the given job on the work queue.
+	 * @param job The job to queue.
+	 */
+	public void queueJob( SatinJob job )
+	{
+		workQueue.insert( job );
+	}
+
+	/**
+	 * Returns the idle time of this processor.
+	 * @return The idle time.
+	 */
+	public double getIdleTime()
+	{
+		return idleTime;
+	}
+
+	/** Returns the slowdown of this processor. The slowdown is
+	 * the multiplication factor for execution times on this processor.
+	 * Slow processors, compared to the reference, have a factor
+	 * above 1, fast processors have a slowdown below 1.
+	 * 
+	 * @return The slowdown of this processor.
+	 */
+	public double getSlowdown()
+	{
+		return slowdown;
+	}
 }
