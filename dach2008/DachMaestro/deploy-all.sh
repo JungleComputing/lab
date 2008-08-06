c96523fb8f9ddd873d8928c9c0dfb7e48728add4
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-08-06 13:43:45 +0000

    + Better deployment tables.

--- dach2008/DachMaestro/deploy-all.sh
+++ dach2008/DachMaestro/deploy-all.sh
@@ -4,7 +4,7 @@
 #
 export GXP_SESSION=`gxpc --create_daemon 1`
 trap 'gxpc quit; exit 1' 2
-HOSTS="suzuk000.intrigger.titech.ac.jp chiba000.intrigger.nii.ac.jp hongo000.logos.ic.i.u-tokyo.ac.jp imade000.kuis.kyoto-u.ac.jp kyoto000.para.media.kyoto-u.ac.jp okubo000.yama.info.waseda.ac.jp mirai000.intrigger.jp kobe000.intrigger.scitec.kobe-u.ac.jp kyushu000.bioinfo.kyushu-u.ac.jp hiro000.net.hiroshima-u.ac.jp keio000.sslab.ics.keio.ac.jp"
+HOSTS="nimbus.titech.hpcc.jp suzuk000.intrigger.titech.ac.jp chiba000.intrigger.nii.ac.jp hongo000.logos.ic.i.u-tokyo.ac.jp imade000.kuis.kyoto-u.ac.jp kyoto000.para.media.kyoto-u.ac.jp okubo000.yama.info.waseda.ac.jp mirai000.intrigger.jp kobe000.intrigger.scitec.kobe-u.ac.jp kyushu000.bioinfo.kyushu-u.ac.jp hiro000.net.hiroshima-u.ac.jp keio000.sslab.ics.keio.ac.jp"
 rm -rf $3
 mkdir $3
 gxpc use ssh '' ''
