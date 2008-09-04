cd7c7613220dd6a420f073489b046f314762cb61
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-09-04 09:45:39 +0000

    + Print file copying stats.

--- dach2008/DachMaestro/copy-and-compare.sh
+++ dach2008/DachMaestro/copy-and-compare.sh
@@ -6,8 +6,12 @@ set -e -u
 DIR=/tmp/dach001-scratch-$$
 #COMPARATOR=/home/dach/finder/dach.sh
 COMPARATOR=./robust-dach.sh
+echo -n STARTCOPY $1 ' '
+date --rfc-3339=ns
 mkdir $DIR
 cp $1 $DIR/file1.fits
 cp $2 $DIR/file2.fits
+echo -n ENDCOPY $1 ' '
+date --rfc-3339=ns
 $COMPARATOR -w $DIR $DIR/file1.fits $DIR/file2.fits
 rm -rf $DIR
